862e717c8f7b330db6554624429ea68c6ae29980
Aaron Martin <notaaronmartin@protonmail.com>
2026-03-30 18:24:02 -0700

    adding vesktop autostart

--- modules/users.nix
+++ modules/users.nix
@@ -15,4 +15,17 @@
 
   # SSH Agent for your GitHub workflow
   programs.ssh.startAgent = true;
+
+  systemd.user.services.vesktop = {
+    description = "Vesktop Discord Client (Delayed Start)";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    
+    serviceConfig = {
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+      ExecStart = "${pkgs.vesktop}/bin/vesktop --start-minimized";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
 }
