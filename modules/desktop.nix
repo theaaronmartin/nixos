f9a34004e637d87a6f34a3153e3296c9688e5e40
Aaron Martin <notaaronmartin@protonmail.com>
2026-03-31 20:55:25 -0700

    updating vesktop

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,6 +4,12 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+    config.common.default = "kde";
+  };
+
   # Keyboard layout
   services.xserver.xkb = {
     layout = "us";
@@ -24,7 +30,12 @@
     
     serviceConfig = {
       ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-      ExecStart = "${pkgs.vesktop}/bin/vesktop --start-minimized";
+      ExecStart = ''
+      ${pkgs.vesktop}/bin/vesktop \
+        --start-minimized \
+        --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer \
+        --ozone-platform=wayland
+      '';
       Restart = "on-failure";
     };
   };
