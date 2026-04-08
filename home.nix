812fc78780e49d84c7e12c829e72fd20b46dcf0c
Aaron Martin <notaaronmartin@protonmail.com>
2026-04-07 20:13:54 -0700

    adding ollama to openclaude

--- home.nix
+++ home.nix
@@ -24,6 +24,9 @@
     vstsync = "yabridgectl sync";
     oc-deep = "CLAUDE_CODE_USE_OPENAI=1 OPENAI_API_KEY=$DEEPSEEK_KEY OPENAI_BASE_URL='https://api.deepseek.com/v1' OPENAI_MODEL='deepseek-chat' /home/plague/.npm-packages/bin/openclaude";
     oc-sonnet = "OPENAI_API_KEY=$ANTHROPIC_KEY /home/plague/.npm-packages/bin/openclaude";
+    oc-llama = "CLAUDE_CODE_USE_OPENAI=1 OPENAI_BASE_URL='http://localhost:11434/v1' OPENAI_MODEL=llama3.1:8b";
+    oc-phi = "CLAUDE_CODE_USE_OPENAI=1 OPENAI_BASE_URL='http://localhost:11434/v1' OPENAI_MODEL=phi3:mini";
+    oc-code = "CLAUDE_CODE_USE_OPENAI=1 OPENAI_BASE_URL='http://localhost:11434/v1' OPENAI_MODEL=codellama:7b";
 
     # Ollama aliases
     ollama-status = "sudo systemctl status ollama";
