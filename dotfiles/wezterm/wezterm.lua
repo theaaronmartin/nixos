04df448755499b7aa0b3cd971b7097d9b897d7bf
Aaron Martin <notaaronmartin@protonmail.com>
2026-04-18 14:03:57 -0700

    updating wezterm tabs

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -43,4 +43,26 @@ config.font_size = 11
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 
+local TAB_WIDTH = 18
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, cfg, hover, max_width)
+    local title = tab.tab_title
+    if not title or #title == 0 then
+        title = tab.active_pane.title
+    end
+    if not title or #title == 0 then
+        local prog = tab.active_pane.foreground_process_name
+        title = prog and prog:match("([^/]+)$") or "shell"
+    end
+    if #title > TAB_WIDTH then
+        title = title:sub(1, TAB_WIDTH)
+    else
+        local padding = TAB_WIDTH - #title
+        local left = math.floor(padding / 2)
+        local right = padding - left
+        title = string.rep(" ", left) .. title .. string.rep(" ", right)
+    end
+    return title
+end)
+
 return config
